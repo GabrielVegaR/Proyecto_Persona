0f8dd6f17ab726a22ff3408c0d1ed1adc2113c50
gabriel <gabrielvegarosario@gmail.com>
2024-08-14 17:04:35 -0400

    Se crearon los DTO para las personas

---
+++ DTOS/ActualizarPersonasDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace Proyecto_Persona.DTOS
+{
+    public class ActualizarPersonasDTO
+    {
+        public string? PrimerNombre { get; set; }
+        public string? SegundoNombre { get; set; }
+        public string? PrimerApellido { get; set; } 
+        public string? SegundoApellido { get; set; }
+        public string? Email { get; set; }
+        public string? TipoDocumento { get; set; }
+        public string? NumDocumento { get; set; }
+        public int? CodigoArea { get; set; }
+        public int? CodigoPais { get; set; }
+        public int? Telefono { get; set; }
+        public string? NumCasa { get; set; }
+        public string? NombreCalle { get; set; }
+        public string? NombreCiudad { get; set; }
+        public string? NombrePais { get; set; }
+        public int? DiaNacimiento { get; set; }
+        public int? MesNacimiento { get; set; }
+        public int? AñoNacimiento { get; set; }
+    }
+}
---
+++ DTOS/PersonaDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace Proyecto_Persona.DTOS
+{
+    public class PersonaDTO
+    {
+        public int Id { get; set; }
+        public string PrimerNombre { get; set; } = null!;
+        public string? SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; } = null!;
+        public string SegundoApellido { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string TipoDocumento { get; set; } = null!;
+        public string NumDocumento { get; set; } = null!;
+        public int CodigoArea { get; set; }
+        public int CodigoPais { get; set; }
+        public int Telefono { get; set; }
+        public string NumCasa { get; set; } = null!;
+        public string NombreCalle { get; set; } = null!;
+        public string NombreCiudad { get; set; } = null!;
+        public string NombrePais { get; set; } = null!;
+        public int DiaNacimiento { get; set; }
+        public int MesNacimiento { get; set; }
+        public int AñoNacimiento { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
