075024d9e2ee155404231a89385eaff6bd067dd2
gabriel <gabrielvegarosario@gmail.com>
2024-08-14 18:29:58 -0400

    Se agrego la identidad de la base de datos para referenciar el id con entity framework

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_Persona.Models;
 
 public partial class Persona
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public string PrimerNombre { get; set; } = null!;
