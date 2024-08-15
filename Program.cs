abd917fc063dedef285a0f05f648ab35b748f01f
gabriel <gabrielvegarosario@gmail.com>
2024-08-14 22:07:45 -0400

    se agregaron los servicios al main y se establecio la coneccion a la base de datos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Persona.Abstraciones.Repositorios;
+using Proyecto_Persona.Abstraciones.Servicios;
+using Proyecto_Persona.Implementaciones.Repositorios;
+using Proyecto_Persona.Implementaciones.Servicios;
+using Proyecto_Persona.Models;
+
 namespace Proyecto_Persona
 {
     public class Program
@@ -13,6 +20,11 @@ namespace Proyecto_Persona
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<PersonasContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IRepositorioPersona, RepositorioPersona>();
+            builder.Services.AddScoped<IServicioPersona, ServicioPersona>();
 
             var app = builder.Build();
 
